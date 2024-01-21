68f9b51ea2230ed07fcd69cbc91cc5a9e4400e5e
hoduulmu <timewarp96@naver.com>
2024-01-21 14:12:42 +0900

    Init Project

---
+++ src/main/java/dream/dreamery/DreameryApplication.java
@@ -0,0 +1,13 @@
+package dream.dreamery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreameryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreameryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dream/dreamery/DreameryApplicationTests.java
@@ -0,0 +1,13 @@
+package dream.dreamery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreameryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
